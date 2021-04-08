31dafd612975b998bbdc930d3b18bd9386939494
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2021-04-08 23:23:26 +0900

    fabric を導入して適当に動かす状態まで

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,43 @@
 <template>
-  <div>Hello, Markee service</div>
+  <h1>Hello, Markee Service!!</h1>
+  <button @click="add">Add</button>
+  <div class="wrapper">
+    <canvas id="canvas" width="600" height="600" />
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { ref, defineComponent, onMounted } from "vue";
+import { fabric } from "fabric";
 
-export default defineComponent({});
+export default defineComponent({
+  setup() {
+    const canvas = ref<fabric.Canvas>(new fabric.Canvas(""));
+
+    onMounted(() => {
+      canvas.value = new fabric.Canvas("canvas");
+    });
+
+    const add = () => {
+      canvas.value.add(
+        new fabric.Rect({
+          top: 100,
+          left: 100,
+          width: 60,
+          height: 60,
+          fill: "red",
+        })
+      );
+    };
+    return { add };
+  },
+});
 </script>
+
+<style scoped>
+.wrapper {
+  width: 600px;
+  height: 600px;
+  border: 1px solid;
+}
+</style>
